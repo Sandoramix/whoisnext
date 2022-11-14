30c3b10aab96bdd42cebc538586be8690081494d
sandoramix <sandoramixer61@gmail.com>
2022-11-14 15:42:14 +0100

    added custom html header

---
+++ src/pages/calendar.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+
+const Calendar: NextPage = () => {
+	return (
+		<>
+			<div className="w-full h-full">
+				Calendar page
+			</div>
+		</>
+	);
+};
+
+export default Calendar;
+
