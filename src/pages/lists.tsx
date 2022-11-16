fac27b8f9fe57004cc7970be62fde1ee8cbf20e8
sandoramix <sandoramixer61@gmail.com>
2022-11-16 23:28:24 +0100

    cleaned code

--- src/pages/lists.tsx
+++ src/pages/lists.tsx
@@ -1,4 +1,3 @@
-import type { Dispatch, SetStateAction } from "react";
 import { useState } from "react";
 import { AiOutlineEdit } from 'react-icons/ai';
 import CreateList from "../components/CreateList";
@@ -6,27 +5,27 @@ import LayerOver from "../components/layerOver";
 import ListDetails from "../components/ListDetails";
 
 import { useLists } from '../lib/ListsContext';
-import { ListItem } from '../types';
+import { List } from '../types';
 
 
 
 export default function ListsPage() {
 
 	const { lists } = useLists();
-	const [openedListIndex, setOpenedListIndex] = useState<number | null>(null)
+	const [openedListId, setOpenedListId] = useState<string | null>(null)
 	const [createListPanelOpened, setCreateListPanelOpened] = useState(false)
 
 
 	const closeListDetails = () => {
-		setOpenedListIndex(null)
+		setOpenedListId(null)
 	}
 
 
 	return (
-		<div className="w-full h-full items-center flex flex-col">
+		<div className="flex flex-col items-center w-full h-full">
 			<div className="pt-4" />
 
-			<section className="w-full flex justify-center">
+			<section className="flex justify-center w-full">
 				<button
 					className="px-6 py-2 bg-[#1f2e47] hover:bg-[#2c3f61]  rounded w-1/3 whitespace-nowrap"
 					onClick={() => setCreateListPanelOpened(true)}
@@ -36,8 +35,8 @@ export default function ListsPage() {
 			<div className="pt-6" />
 
 			<div className="max-h-[calc(100vh_-_90px_-_2.5rem)] overflow-auto w-full flex flex-col justify-center items-center">
-				<ul className="list-none w-10/12 py-6 flex flex-col gap-8">
-					{lists?.map((list, index) => <ListItem setOpenedListIndex={setOpenedListIndex} index={index} list={list} key={index} />)}
+				<ul className="flex flex-col w-10/12 gap-8 py-6 list-none">
+					{lists?.map((list, index) => <List openThisList={() => setOpenedListId(list.id)} list={list} key={index} />)}
 				</ul>
 			</div>
 
@@ -45,9 +44,9 @@ export default function ListsPage() {
 				createListPanelOpened ? <LayerOver closeView={() => setCreateListPanelOpened(false)}>
 					<CreateList closeView={() => setCreateListPanelOpened(false)} />
 				</LayerOver>
-					: openedListIndex !== null && <LayerOver closeView={closeListDetails}>
+					: openedListId !== null && <LayerOver closeView={closeListDetails}>
 						<ListDetails
-							currentListIndex={openedListIndex}
+							currentListId={openedListId}
 							closeView={closeListDetails}
 						/>
 					</LayerOver>
@@ -57,21 +56,21 @@ export default function ListsPage() {
 	)
 }
 
-const ListItem = ({ list, setOpenedListIndex, index }: { list: ListItem, index: number, setOpenedListIndex: Dispatch<SetStateAction<number | null>> }) => {
+const List = ({ list, openThisList }: { list: List, openThisList: () => void }) => {
 
 	const onEditClick = () => {
-		setOpenedListIndex(index)
+		openThisList()
 	}
 
 	return (
 		<li className="relative bg-[#000d22] overflow-visible rounded select-none">
-			<h2 className="absolute -top-4 left-1/2 -translate-x-1/2 text-xl font-serif font-bold">{list.title}</h2>
+			<h2 className="absolute font-serif text-xl font-bold -translate-x-1/2 -top-4 left-1/2">{list.title}</h2>
 			<div className="flex justify-between min-h-[100px] items-center px-4 text-center">
-				<div className="text-gray-300 text-sm">{list.people.length} <br />People</div>
-				<div className="text-emerald-400 text-sm">{list.people.filter(p => p.isCompleted).length} <br />Completed</div>
+				<div className="text-sm text-gray-300">{list.people.length} <br />People</div>
+				<div className="text-sm text-emerald-400">{list.people.filter(p => p.isCompleted).length} <br />Completed</div>
 				<div
 					onClick={onEditClick}
-					className="text-xl font-bold flex flex-col justify-center items-center text-yellow-500 cursor-pointer hover:text-yellow-300"
+					className="flex flex-col items-center justify-center text-xl font-bold text-yellow-500 cursor-pointer hover:text-yellow-300"
 				>
 					<AiOutlineEdit className="text-3xl" />
 					<h3>Edit</h3>
