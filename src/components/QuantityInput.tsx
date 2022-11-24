cc9e6af0e1314eec26477ae5d9450c4d53841ddd
sandoramix <sandoramixer61@gmail.com>
2022-11-24 01:07:27 +0100

    refactored

--- src/components/QuantityInput.tsx
+++ src/components/QuantityInput.tsx
@@ -1,19 +1,23 @@
 
 import type { FC } from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { type List } from '../types';
 import { getPeopleCount } from '../utils/lists';
 
 type QuantityInputProps = {
 
 	list?: List,
-	onlyIncompleteList?: boolean,
-	onChange: (val: string) => void,
+	onlyIncompletePeople?: boolean,
+	setValue: (val: string) => void,
+	fullWidth?: boolean
 }
 
 
-const QuantityInput: FC<QuantityInputProps> = ({ onChange, list, onlyIncompleteList }) => {
+const QuantityInput: FC<QuantityInputProps> = ({ setValue, list, onlyIncompletePeople, fullWidth }) => {
 	const inputRef = useRef<HTMLInputElement>(null)
+
+
+	const peopleCount = useMemo(() => !list ? -1 : getPeopleCount(list, onlyIncompletePeople), [list, onlyIncompletePeople])
 
 
 	useEffect(() => {
@@ -21,39 +25,40 @@ const QuantityInput: FC<QuantityInputProps> = ({ onChange, list, onlyIncompleteL
 		inputRef.current.value = ``
 	}, [list])
 
-	function fixNumber(value: string) {
+	function fixQuantity(value: string) {
 		const quantity = parseInt(value);
-		if (!list || getPeopleCount(list, onlyIncompleteList) === 0 || value.trim() === ``) return "";
-		return String((quantity > getPeopleCount(list, onlyIncompleteList)) ? getPeopleCount(list, onlyIncompleteList) : (quantity < 1) ? 1 : quantity)
+		if (!list || peopleCount === 0 || value.trim() === ``) return "";
+		return String((quantity > peopleCount) ? peopleCount : (quantity < 1) ? 1 : quantity)
+	}
+
+	function handleQuantity(value: string, element: HTMLInputElement) {
+		const finalValue = fixQuantity(value)
+		setValue(finalValue)
+		element.value = finalValue
 	}
 
 	function onInputChange(ev: React.ChangeEvent<HTMLInputElement>) {
-		const value = ev.currentTarget.value;
-		const finalValue = fixNumber(value)
-		onChange(finalValue)
-		ev.currentTarget.value = finalValue
+		const value = ev.currentTarget.value.trim();
+		handleQuantity(value, ev.currentTarget)
 	}
 
 	function incrementQuantity(n: number) {
 		if (!inputRef.current) return;
 
 		const value = inputRef.current.value.trim();
-		const number = (value === `` ? 0 : parseInt(value)) + n;
-
-		const finalValue = fixNumber(number.toString())
-		onChange(finalValue)
-		inputRef.current.value = finalValue
+		const number = value === `` ? 0 : parseInt(value) + n;
+		handleQuantity(number.toString(), inputRef.current)
 	}
 
 
 	return (
-		<div id="quantity-input" className='relative w-3/4 max-w-xs h-14  min-w-[150px]  text-xl bg-white/10 px-[2ch] rounded focus-within:outline-white border border-white/10'>
-			<button className='absolute top-1/2 left-0 -translate-y-1/2 text-4xl font-bold text-red-600 hover:text-red-500 p-1 disabled:text-gray-500'
+		<div id="quantity-input" className={`relative ${fullWidth ? `w-full` : `w-1/2`} grow h-14  text-xl bg-white/10 px-[2ch] rounded focus-within:outline-white border border-white/10`}>
+			<button className='absolute left-0 p-1 text-4xl font-bold text-red-600 -translate-y-1/2 top-1/2 hover:text-red-500 disabled:text-gray-500'
 				onClick={() => incrementQuantity(-1)}
-				disabled={!list || getPeopleCount(list, onlyIncompleteList) === 0}
+				disabled={!list || peopleCount === 0}
 			>-</button>
 			<input
-				disabled={!list || getPeopleCount(list, onlyIncompleteList) === 0}
+				disabled={!list || peopleCount === 0}
 				onChange={onInputChange}
 				onInput={onInputChange}
 				autoComplete="off"
@@ -61,12 +66,12 @@ const QuantityInput: FC<QuantityInputProps> = ({ onChange, list, onlyIncompleteL
 				name="name"
 				id="name"
 				ref={inputRef}
-				className="bg-transparent text-center text-base h-full w-full text-white placeholder:text-center placeholder:uppercase focus:outline-none disabled:line-through"
+				className="w-full h-full text-base text-center text-white bg-transparent placeholder:text-center placeholder:uppercase focus:outline-none disabled:line-through"
 				placeholder='Quantity'
 			/>
-			<button className='absolute top-1/2 right-0 -translate-y-1/2 text-4xl font-bold text-green-600 hover:text-green-500 p-1 disabled:text-gray-500'
+			<button className='absolute right-0 p-1 text-4xl font-bold text-green-600 -translate-y-1/2 top-1/2 hover:text-green-500 disabled:text-gray-500'
 				onClick={() => incrementQuantity(1)}
-				disabled={!list || getPeopleCount(list, onlyIncompleteList) === 0}
+				disabled={!list || peopleCount === 0}
 			>+</button>
 		</div>
 	)
