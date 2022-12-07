35b8b598f133acacc6991ce07afd050ac3bdc268
Sandoramix <sandoramixer61@gmail.com>
2022-12-07 10:02:32 +0100

    removed index, calendar pages, renamed repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 
 import Link from 'next/link';
 import { useRouter } from "next/router";
-import { BiHome } from "react-icons/bi";
 import { ListsProvider } from "../lib/ListsContext";
 import "../styles/globals.css";
 
@@ -13,22 +12,22 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Pickadate</title>
+        <title>WhoIsNext</title>
         <meta name="description" content="Website designed to extract a random person from the list for school oral exams (Welcome to Italy).It also the possibility of creating a calendar until the end of the month of all the students on a specific list." />
         <link rel="icon" href="/logo.png" />
       </Head>
 
       <div className="w-screen min-h-[600px] overflow-x-hidden h-screen bg-html text-white font-main text-base relative">
-        <header className="overflow-hidden bg-header/25 w-full max-h-header h-header grid-flow-col grid grid-cols-3 justify-center items-center text-center border-b border-cyan-800/30 font-sub">
+        <header className="overflow-hidden bg-header/25 w-full max-h-header h-header grid-flow-col grid grid-cols-2 justify-center items-center text-center border-b border-cyan-800/30 font-sub">
 
           <Link href={`/lists`} className="px-4 py-1 min-w-fit hover:text-cyemerald hover:text-xl transition-all duration-300 text-center ">
             <h1 className={pathname == `/lists` ? `text-cyemerald [text-shadow:_2px_2px_5px_#080]` : ``}>Lists</h1>
           </Link>
-          <Link href={`/`} className="flex justify-center items-center px-4 py-1 min-w-fit hover:text-cyemerald text-3xl hover:text-4xl transition-all duration-300 text-center">
+          {/* <Link href={`/`} className="flex justify-center items-center px-4 py-1 min-w-fit hover:text-cyemerald text-3xl hover:text-4xl transition-all duration-300 text-center">
             <h1 className={pathname == `/` ? `text-cyemerald [text-shadow:_2px_2px_5px_#080]` : ``}>
               <BiHome />
             </h1>
-          </Link>
+          </Link> */}
           <Link href={`/pick`} className="px-4 py-1 min-w-fit hover:text-cyemerald hover:text-xl transition-all duration-300 text-center">
             <h1 className={pathname == `/pick` ? `text-cyemerald [text-shadow:_2px_2px_5px_#080]` : ``}>Random <br className="block" />Pick</h1>
           </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,21 @@ import type { GetServerSideProps } from "next";
 import { type NextPage } from "next";
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: `/lists`,
-      permanent: false
-    }
-  }
-}
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   return {
+//     redirect: {
+//       destination: `/lists`,
+//       permanent: false
+//     }
+//   }
+// }
 
 const Home: NextPage = () => {
   return (
     <>
       <div className="w-full h-full px-6 py-2">
         <section className="flex justify-center items-center">
-          <h1 className="text-4xl font-serif">Pickadate</h1>
+          <h1 className="text-4xl font-serif">WhoIsNext</h1>
         </section>
         <section>
 
