8b41acd980ea10834fd17868801c5684995f49b0
Sandoramix <sandoramixer61@gmail.com>
2022-12-13 13:51:50 +0100

    implemented list searching (wip)

--- src/pages/lists.tsx
+++ src/pages/lists.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { AiOutlineEdit } from 'react-icons/ai';
+import { BiSearchAlt, BiSearchAlt2 } from 'react-icons/bi';
 import LayerOver from "../components/layerOver";
 import CreateList from "../components/listsPage/CreateList";
 import ListDetails from "../components/listsPage/ListDetails";
@@ -8,11 +9,17 @@ import { useLists } from '../lib/ListsContext';
 import { List } from '../types';
 
 
-
 export default function ListsPage() {
 
+
 	const { lists } = useLists();
-	const [createListPanelOpened, setCreateListPanelOpened] = useState(false)
+	const [createListPanelOpened, setCreateListPanelOpened] = useState(false);
+
+	const [searchedListName, setSearchedListName] = useState("");
+	const filteredList = useMemo(() => [...lists.values()].filter(list => list.title.includes(searchedListName)), [lists, searchedListName]);
+
+	const addBtnRef = useRef<HTMLButtonElement>(null);
+	const searchInputCntRef = useRef<HTMLDivElement>(null);
 
 	const [selectedList, setSelectedList] = useState<List | null>(null)
 	const closeListDetails = () => {
@@ -25,22 +32,48 @@ export default function ListsPage() {
 
 	}
 
+	function toggleSearchBtn() {
+
+		if (!addBtnRef.current) return;
+		const isActive = addBtnRef.current.className.includes('grow');
+
+		addBtnRef.current.textContent = !isActive ? `Add List` : `+`
+		addBtnRef.current.classList.toggle(`grow`, !isActive)
+		addBtnRef.current.classList.toggle(`!text-3xl`, isActive)
+		addBtnRef.current.classList.toggle(`!rounded-full`, isActive)
+
+		if (!searchInputCntRef.current) return;
+		searchInputCntRef.current.classList.toggle('!w-full', isActive)
+
+
+
+
+	}
+
 
 	return (
-		<div className="flex flex-col items-center w-full h-full">
-			<div className="pt-4" />
-
+		<div className="flex flex-col items-center w-full h-full bg-gradient-to-b to-[#0f0c4c] from-[#06001a]">
 			<div className="flex flex-col items-center justify-start w-full overflow-auto max-h-main h-full ">
-				<nav className='flex w-full justify-center px-2 pb-4 py-1 sticky top-0 left-0 z-10 bg-main h-24'>
-					<div className="pt-4" />
+				<nav className='flex gap-2 w-full justify-center items-center px-2 py-16 sticky top-0 left-0 z-10 bg-[#060018] h-28 drop-shadow-xl shadow-md'>
+
 					<button
-						className="text-xl font-flat px-6 py-2 bg-[#002569] hover:bg-[#002b71]  rounded w-full min-w-[300px] max-w-[400px] h-[calc(100vw_*_.05_+_40px)] max-h-[90px] whitespace-nowrap"
+						// bg-[#002569] hover:bg-[#002b71]
+						ref={addBtnRef}
+						className="text-xl font-sub px-6 py-2 bg-indigo-900 hover:bg-indigo-800 grow rounded max-w-[400px] w-unknown aspect-square h-unknown max-h-90px whitespace-nowrap"
 						onClick={() => setCreateListPanelOpened(true)}>
 						Add List
 					</button>
+
+					<div className='flex justify-center items-center h-unknown max-h-90px'>
+						<div ref={searchInputCntRef} className='overflow-hidden w-0 h-unknown flex items-center  max-h-90px '>
+							<input value={searchedListName} onInput={(ev) => setSearchedListName(ev.currentTarget.value ?? "")} type="text" className='bg-transparent border-2 border-white h-full w-full px-2 py-1' />
+						</div>
+						<BiSearchAlt onClick={toggleSearchBtn} className=' text-4xl sm:text-5xl cursor-pointer grow min-w-min max-w-90px max-h-90px h-full rounded aspect-square p-2  bg-cyan-800 hover:bg-cyan-700' />
+					</div>
+
 				</nav>
 				<ul className="flex flex-col w-[95%] min-w-[310px] max-w-[950px] gap-8 py-6 list-none">
-					{[...lists.values()].map((list, index) => <List openThisList={() => openList(list)} list={list} key={index} />)}
+					{filteredList.map((list, index) => <List openThisList={() => openList(list)} list={list} key={index} />)}
 				</ul>
 
 			</div>
@@ -75,10 +108,10 @@ const List = ({ list, openThisList }: { list: List, openThisList: () => void }) 
 	}
 
 	return (
-		<li className="relative  bg-gradient-to-r from-[#0b0031] to-blue-900  overflow-visible rounded select-none">
+		<li className="relative  bg-gradient-to-r to-blue-900  from-cyan-900  overflow-visible rounded select-none">
 			<h2 className="absolute font-serif text-2xl  font-bold -translate-x-1/2 -top-4 left-1/2 whitespace-nowrap">{list.title}</h2>
 			<div className="w-full grid justify-between h-[calc(100vw_*_.05_+_80px)] max-h-[120px] grid-flow-row grid-cols-3 auto-cols-min items-center  px-4 text-center font-medium font-sans">
-				<div className="text-base sm:text-xl lg:text-2xl  text-gray-200 transition-[font-size] duration-200 items-start flex">{list.people.length} <br />People</div>
+				<div className="text-base sm:text-xl lg:text-2xl  text-gray-200 transition-[font-size] duration-200 items-start flex">{list.people.length} <br />{list.people.length == 1 ? `Person` : `People`}</div>
 				<div className="text-base sm:text-xl lg:text-2xl  text-emerald-300 transition-[font-size] duration-200">{list.people.filter(p => p.isCompleted).length} <br />Completed</div>
 				<div
 
